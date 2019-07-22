b0beb41a4fc3e4ddb8297c10b13f99f42e2a09b9
Phillip <phillip@Tecra.home>
2019-07-22 16:27:14 -0400

    come fly away

---
+++ Grapher.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Line2D;
+import java.awt.geom.Path2D;
+import java.util.List;
+import javax.swing.JPanel;
+
+public class Grapher extends JPanel {
+  private int WINDOW_SIZE;
+  private double xScalar;
+  private double vScalar;
+
+  public Grapher(int dimension, double maxx, double maxv){
+    this.WINDOW_SIZE = 2*dimension;
+    xScalar = dimension / maxx;
+    vScalar = dimension / maxv;
+	}
+
+	//Method for painting everything
+	//Note: Method is called twice during initialization
+	public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    this.setBackground(Color.WHITE);
+
+    //draw the axes
+    g.drawLine(0, WINDOW_SIZE, 2*WINDOW_SIZE, WINDOW_SIZE);
+    g.drawLine(WINDOW_SIZE,0 , WINDOW_SIZE, 2*WINDOW_SIZE);
+
+    //draw the data
+    double[][] data = Main.returnData();
+    for(int i = 0; i<data.length; i++){
+      double x = data[i][0]*xScalar;
+      double v = data[i][1]*vScalar;
+
+      g.drawOval(WINDOW_SIZE+(int)(Math.round(x)),WINDOW_SIZE-(int)(Math.round(v)),1,1);
+    }
+  }
+
+  public Dimension getPreferredSize() {
+
+    return new Dimension(2*WINDOW_SIZE, 2*WINDOW_SIZE);
+
+  }
+}
--- Main.java
+++ Main.java
@@ -1,26 +1,84 @@
+import javax.swing.JFrame;
+
 public class Main {
+  //global graphing window variables.
+  static int size = 200; //the positive axis distance in pixels.
+  static double maxx = 2; //max x to be represented on graph
+  static double maxv = 2; //max x to be represented on graph
+  static double[][] Data;
+  static double[][] periodData;
+  static double[][][] allPeriods;
+
   public static void main(String[] args){
+    //user set variables
     double a = 1;
-    double b = 1;
-    double c = 1;
-    double w = 1;
-    double F = 0.855;
-    double x = 0;
+    double b = -1;
+    double c = 0.3;
+    double w = 1.2;
+    double F = 0.5;
+    double x = 1;
     double v = 0;
 
-    double endTime = 10000;
+    int numberData = 1000;
     double resolution = 0.001;
 
+    //run simulation
+    periodData = new double[numberData][3];
+    allPeriods = new double[2+(int)(Math.round(2*Math.PI/(w*resolution)))][numberData+2][3];
+    double endTime = numberData * 2*Math.PI/w;
     Oscillator theOscillator = new Oscillator(a,b,c,w,F,x,v);
-
     double[][] data = theOscillator.evalToTime(endTime,resolution);
-
+    int dataAdded = 0;
+    Data = data;
+    int placeInPeriod = 0;
+    int periodNumber = 0;
     double nextPeriod = 0;
     for(int i = 0; i < data.length; i++){
+      allPeriods[placeInPeriod][periodNumber] = data[i];
       if(data[i][2] >= nextPeriod){
+        dataAdded++;
+        placeInPeriod = 0;
+        periodNumber++;
+        periodData[dataAdded-1] = data[i];
         System.out.println("(" + data[i][2] + ", " + data[i][0] + ", "  + data[i][1] + ")");
-        nextPeriod+=2*Math.PI;
+        nextPeriod+=(2*Math.PI)/w;
+      }
+      placeInPeriod++;
+    }
+
+    System.out.println("data added: " + dataAdded);
+    animatePeriods();
+  }
+
+  public static double[][] returnData(){
+    return Data;
+  }
+
+  public static void graphData(){
+    JFrame frame = new JFrame("Simulation");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.add(new Grapher(size, maxx, maxv));
+    frame.pack();
+    frame.setVisible(true);
+  }
+
+
+  public static void animatePeriods(){
+    Data = allPeriods[0];
+    JFrame frame = new JFrame("Animation");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.add(new Grapher(size, maxx, maxv));
+    frame.pack();
+    frame.setVisible(true);
+
+    while(true){
+      for(double[][] graph : allPeriods){
+        Data = graph;
+        try{ Thread.sleep(1); }
+        catch (Exception exc){}
+        frame.repaint();
       }
     }
   }
+
 }
